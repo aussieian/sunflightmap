943d9443ae138081943d87270959afe9f62a4dab
Ian C's macbook <ian@insight4.com>
2011-10-16 16:01:08 +0800

    got google maps working

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ include("lib/global.php");
 	<title>Sun Flight Map - Map the path of your flight and the sun | built at tnooz tHack Singapore!</title>
 
 	<meta name="description" content="Map the path of your flight and the sun. Built at tnooz tHack Singapore!">
-	<meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
 	<meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
 	
 	<meta property="og:title" content="Sun Flight Map" /> 
@@ -19,16 +19,14 @@ include("lib/global.php");
 	<!-- libraries -->
 	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js"></script>
 	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.9/jquery-ui.min.js"></script>
-	<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=true&language=en"></script>
+	<script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?sensor=false"></script>
 
 	<!-- custom code -->
 	<script type="text/javascript" src="/javascript/main.js"></script>
 	
 </head>
 <body>
-	<div id="header">This is the header</div>
-	<div id="map-canvas">This is the google map</div>
-	<div id="footer">This is the footer</div>
+	<div id="map_canvas">Loading...</div>
 	<div id="debug">
 	<?php
 		$airport = getAiport("SYD");
--- javascript/main.js
+++ javascript/main.js
@@ -3,44 +3,16 @@ $(document).ready(function() {
 	var map;
 
 	function initializeMap() {
-
-		// check if we have google loaded
-		if (typeof google === 'undefined') {
-			alert("Sorry, we're having trouble loading Google Maps. The flight map will be disabled.");
-			return;
-		}
-
-		//try {
-			startLatLng = new google.maps.LatLng("50,50");
-			var myOptions = {
-				disableDefaultUI: false,
-				scaleControl: true,
-				streetViewControl: false,
-				panControl: true,
-				panControlOptions: {
-					position: google.maps.ControlPosition.LEFT_CENTER
-				},
-				zoomControlOptions: {
-					position: google.maps.ControlPosition.LEFT_CENTER
-				},
-				maxZoom: 8,
-				minZoom: 1,
-				mapTypeControl: false,
-				scrollwheel: false,
-				zoom: 4,
-				center: startLatLng,
-				mapTypeId: google.maps.MapTypeId.TERRAIN
-			}
-			map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
-			
-		//} catch(err) {
-		//	alert("Sorry, we're having trouble loading Google Maps. Please try again later.");
-		//}
-
+	        var myOptions = {
+	          zoom: 8,
+	          center: new google.maps.LatLng(-34.397, 150.644),
+	          mapTypeId: google.maps.MapTypeId.ROADMAP
+	        };
+	        map = new google.maps.Map(document.getElementById('map_canvas'), myOptions);
 	}
-	
-	function main() { 
-		initializeMap();
+
+	function main() {
+		google.maps.event.addDomListener(window, 'load', initializeMap);
 	}
 	
 	// let's do it!
