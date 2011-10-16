58996bbe9e4d170fe9c271fd9dccffd0d105c066
Dan C <dan@dsync.org>
2011-10-16 18:36:27 +0200

    added some example flight codes

--- lib/config.php
+++ lib/config.php
@@ -5,6 +5,6 @@ $cfg["SITE_NAME"] = "Sun Flight Map";
 $cfg["OAG_USERNAME"] = "THACK";
 $cfg["OAG_PASSWORD"] = "THACK";
 $cfg["PHP_TIMEZONE_OFFSET"] = 8; // Singapore GMT+8
-$cfg["EXAMPLE_FLIGHT_ROUTES"] = array("JQ7", "QF007");
+$cfg["EXAMPLE_FLIGHT_ROUTES"] = array("JQ7", "JQ8", "QF007", "SA287", "JQ25", "CX7800", "SQ618", "BI421", "EVA25");
 
 ?>
