5b17eac593c7831e04aa2cc6a363f62f3661018e
Ian C's macbook <ian@insight4.com>
2011-10-16 15:41:38 +0800

    udpate

--- lib/config.php
+++ lib/config.php
@@ -2,5 +2,7 @@
 
 $cfg = array();
 $cfg["SITE_NAME"] = "Sun Flight Map";
+$cfg["OAG_USERNAME"] = "THACK";
+$cfg["OAG_PASSWORD"] = "THACK";
 
 ?>
---
+++ lib/test.php
@@ -0,0 +1,11 @@
+<form method="POST" action="http://ondemandtestharness.oag.com/CBWSTestHarnessPublic//FlightLookupRequestAction.do?">
+	<input type='textfield' name='actionForm' value='FlightLookupRequestForm'>
+	<input type='textfield' name='inputPrefix' value='f_'>
+	<input type='textfield' name='f_username' value='THACK'>
+	<input type='textfield' name='f_password' value='THACK'>
+	<input type='textfield' name='f_carrierCode' value='JQ'>
+	<input type='textfield' name='f_serviceNumber' value='7'>
+	<input type='textfield' name='f_requestDate' value='2011-10-16'>
+	<input type='textfield' name='f_requestTime' value='12:00:00'>
+	<input type='submit'>
+</form>
