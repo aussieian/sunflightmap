c96150607f81fd994d918a4929d04f4da1827244
Ian C's macbook <ian@insight4.com>
2011-10-26 21:17:50 +1100

    update password

--- lib/config.php
+++ lib/config.php
@@ -2,8 +2,8 @@
 
 $cfg = array();
 $cfg["SITE_NAME"] = "Sun Flight Map";
-$cfg["OAG_USERNAME"] = "THACK";
-$cfg["OAG_PASSWORD"] = "THACK";
+$cfg["OAG_USERNAME"] = "GetFlight"; // THACK
+$cfg["OAG_PASSWORD"] = "GetFlight"; // THACK
 $cfg["PHP_TIMEZONE_OFFSET"] = 8; // Singapore GMT+8
 $cfg["EXAMPLE_FLIGHT_ROUTES"] = array("JQ7", "JQ8", "QF007", "SA287", "JQ25", "CX7800", "SQ618", "BI421", "EVA25", "LAN833");
 $cfg["HYPNOTOAD_URL"] = "http://www.wego.com/hotels/united-arab-emirates/dubai/burj-al-arab--85149?search_id=5426437";
