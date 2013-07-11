2cbbbb37b0718f6f75dd8558b8a58dffed21b010
Ian C <ian@insight4.com>
2013-07-12 04:38:29 +1000

    added js

---
+++ lib/config-sunflight.net.php
@@ -0,0 +1,10 @@
+<?php
+
+$cfg = array();
+$cfg["SITE_NAME"] = "Sun Flight Map";
+$cfg["OAG_USERNAME"] = "GetFlight"; // THACK
+$cfg["OAG_PASSWORD"] = "tdrtkp2"; // THACK
+$cfg["PHP_TIMEZONE_OFFSET"] = 10; // Sydney GMT+10
+$cfg["EXAMPLE_FLIGHT_ROUTES"] = array("BA5", "QF1", "QF9", "UA154", "UA172");
+$cfg["SFCALC_CMD"] = "/var/www/sunflight/sfcalc/sfcalc.py";
+?>
---
+++ lib/config.php
@@ -0,0 +1,8 @@
+<?php
+
+$config_name = str_replace("www", "", $_SERVER["HTTP_HOST"]);
+@include_once("lib/config-" . $config_name . ".php");
+@include_once("../lib/config-" . $config_name . ".php");
+@include_once("../../lib/config-" . $config_name . ".php");
+
+?>
