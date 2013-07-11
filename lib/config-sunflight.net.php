185371a5a4862759fe7cc265327d73f19d07649e
Ian C <ian@insight4.com>
2013-07-12 04:47:00 +1000

    add sfcalc test

--- lib/config-sunflight.net.php
+++ lib/config-sunflight.net.php
@@ -6,5 +6,5 @@ $cfg["OAG_USERNAME"] = "GetFlight"; // THACK
 $cfg["OAG_PASSWORD"] = "tdrtkp2"; // THACK
 $cfg["PHP_TIMEZONE_OFFSET"] = 10; // Sydney GMT+10
 $cfg["EXAMPLE_FLIGHT_ROUTES"] = array("BA5", "QF1", "QF9", "UA154", "UA172");
-$cfg["SFCALC_CMD"] = "/var/www/sunflight/sfcalc/sfcalc.py";
+$cfg["SFCALC_CMD"] = "/var/www/sunflight.net/sfcalc/sfcalc.py";
 ?>
