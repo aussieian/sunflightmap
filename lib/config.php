ae36672b0424ea03c77cf2cf53464ad2276c6e62
root <root@editsiteonline.com>
2013-07-12 18:20:26 +1000

    update config for www

--- lib/config.php
+++ lib/config.php
@@ -1,6 +1,6 @@
 <?php
 
-$config_name = str_replace("www", "", $_SERVER["HTTP_HOST"]);
+$config_name = str_replace("www.", "", $_SERVER["HTTP_HOST"]);
 @include_once("lib/config-" . $config_name . ".php");
 @include_once("../lib/config-" . $config_name . ".php");
 @include_once("../../lib/config-" . $config_name . ".php");
